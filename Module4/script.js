0e61e3bf3952f5d46ebb9a06fee72254b6c375b5
Ali Hajipour <alihajipour.it@gmail.com>
2023-04-29 15:21:59 +0430

    updated

--- Module4/script.js
+++ Module4/script.js
@@ -1,15 +1,15 @@
 var names = new Array();
 
-names[0] = "Jacob";
+names[0] = "Yaakov";
 names[1] = "John";
-names[2] = "Alfred";
-names[3] = "Nancy";
-names[4] = "Sara";
-names[5] = "Alexander";
-names[6] = "Jamie";
-names[7] = "Mike";
-names[8] = "Jade";
-names[8] = "Ali";
+names[2] = "Jen";
+names[3] = "Jason";
+names[4] = "Paul";
+names[5] = "Frank";
+names[6] = "Larry";
+names[7] = "Paula";
+names[8] = "Laura";
+names[8] = "Jim";
 
 for (var i =0 ; i<names.length; i++) {
     if(names[i].charAt(0) === "J" || names[i].charAt(0) === 'j') {
